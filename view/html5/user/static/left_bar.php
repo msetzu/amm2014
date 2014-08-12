c999b4789887c53bc8bf1adfba424fde3ef0a9d1
Setzu-Mattia <ma.setzu2@studenti.unica.it>
2014-08-12 18:17:54 +0200

    Forgot to add profile edit link

--- view/html5/user/static/left_bar.php
+++ view/html5/user/static/left_bar.php
@@ -1,9 +1,13 @@
 <div class="left_bar">
     
     <img src="../../icons/user_logo.svg"/>
+    
     <form action="./test.php" method="post">
+        
         <input type="image" src="../../icons/patients.svg" name="display" value="patients"/>
-        <input type="image" src="../../icons/profile.svg" name="display" value="profile"/>        
+        <input type="image" src="../../icons/profile.svg" name="display" value="profile"/>
+        <input type="image" src="../../icons/edit.svg" name="display" value="edit?profile"/>
+        
     </form>
     
 </div>
