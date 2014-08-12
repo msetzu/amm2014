77fa3409116003b41aa94c44fb7eb474aef7ef9a
Setzu-Mattia <ma.setzu2@studenti.unica.it>
2014-08-13 00:08:15 +0200

    Added search link

--- view/html5/user/static/left_bar.php
+++ view/html5/user/static/left_bar.php
@@ -6,6 +6,7 @@
         
         <input type="image" src="../../icons/patients.svg" name="display" value="patients"/>
         <input type="image" src="../../icons/profile.svg" name="display" value="profile"/>
+        <input type="image" src="../../icons/add_white.svg" name="display" value="profile"/>
         <input type="image" src="../../icons/edit.svg" name="display" value="edit?profile"/>
         
     </form>
